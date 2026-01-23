2c286fa4dc654b3c0825bb892034180d415d4722
MarcoQAestudos <estdosti-marco@outlook.com>
2026-01-23 13:28:42 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,26 +1,27 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert
+
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  //check point 1: verificar se o titulo da pagina é "Velô Sprint"
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+   
+    //Arrange
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByTestId('header-nav')).toContainText('Consultar Pedido');
 
-  //check point 2: verificar se a pagina de consulta de pedidos é carregada
-  await expect(page.getByTestId('header-nav')).toContainText('Consultar Pedido');
+    //Act
+    await page.getByTestId('search-order-id').fill('VLO-7AKU91');
+    await page.getByTestId('search-order-button').click();
 
-  //numero do pedido valido  
-  await page.getByTestId('search-order-id').fill('VLO-7AKU91');
+    //Assert
+    await expect(page.getByTestId('order-result-status')).toBeVisible();
+    await expect(page.getByTestId('order-result-id')).toContainText('VLO-7AKU91');
 
-  await page.getByTestId('search-order-button').click();
+    await expect(page.getByTestId('order-result-status')).toBeVisible();
+    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-7AKU91');
-
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-       
 
 
 });
