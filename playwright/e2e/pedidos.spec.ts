ab075ba7844ddb60eca97e1d2df670bf580a6547
MarcoQAestudos <estdosti-marco@outlook.com>
2026-01-26 16:55:11 -0300

    Seafio apagar o order-result-id e order-result-status e refazer o teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -15,15 +15,18 @@ test('test', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-7AKU91');
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
-     //Assert era assim
-    //await expect(page.getByTestId('order-result-id')).toBeVisible({timeout:10_000});
-    //await expect(page.getByTestId('order-result-id')).toContainText('VLO-7AKU91');
+    //Assert era assim
+    // await expect(page.getByTestId('order-result-id')).toBeVisible({timeout:10_000});
+    // await expect(page.getByTestId('order-result-id')).toContainText('VLO-7AKU91');
 
-    // await expect(page.getByTestId('order-result-status')).toBeVisible();
-    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+    //  await expect(page.getByTestId('order-result-status')).toBeVisible();
+    //  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
-    //Assert Ficou assim
-    await expect(page.getByTestId('search-order-button')).toContainText('Buscar Pedido');
-    await expect(page.getByTestId('order-result-VLO-7AKU91')).toContainText('APROVADO');
-    
+    //Assert Ficou assim assim
+    await expect(page.getByTestId('search-order-button')).toBeVisible({timeout:10_000});
+    await expect(page.getByRole('textbox', { name: 'Número do Pedido' })).toHaveValue('VLO-7AKU91');
+    await expect(page.getByText('APROVADO')).toBeVisible();
+
+   
+
 });
